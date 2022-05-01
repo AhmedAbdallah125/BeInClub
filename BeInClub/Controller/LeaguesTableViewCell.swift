48b349ca4af277a6439ed71fdd681f844a8c9361
Mohamed Hosam eldien <mohamedhosameldien07@gmail.com>
2022-05-01 02:54:26 +0200

    create leagues Ui screen

---
+++ BeInClub/Controller/LeaguesTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  LeaguesTableViewCell.swift
+//  BeInClub
+//
+//  Created by Mohamed Hosam on 01/05/2022.
+//
+
+import UIKit
+
+class LeaguesTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var leagueImage: UIImageView!
+    
+    @IBOutlet weak var leagueName: UILabel!
+    
+    @IBOutlet weak var leagueYoutube: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
