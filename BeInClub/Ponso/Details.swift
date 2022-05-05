fee2a23800a37ef671d69a04e86a8817d077d87e
Abdulrahman.ali19 <Abdulrahman.ali19@outlook.com>
2022-05-06 01:14:27 +0200

    fixing some issues, init details screen

---
+++ BeInClub/Ponso/Details.swift
@@ -0,0 +1,18 @@
+//
+//  Details.swift
+//  BeInClub
+//
+//  Created by Manny on 06/05/2022.
+//
+
+import Foundation
+
+struct Details {
+    let name : String
+    let logoUrl : String
+    let league : String
+    let stadiumName : String
+    let stadiumImgUrl : String
+    let country : String
+    let description : String
+}
