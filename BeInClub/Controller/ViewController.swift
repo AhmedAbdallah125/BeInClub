3ca9a1b431ef7c801650102af3dcca7b56d69579
AbdelrahmanSayed19598 <abdosayed20162054@gmail.com>
2022-05-09 17:49:52 +0200

    launch_screen_and_app_icon

--- BeInClub/Controller/ViewController.swift
+++ BeInClub/Controller/ViewController.swift
@@ -6,11 +6,25 @@
 //
 
 import UIKit
-
+import Lottie
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let animationView = AnimationView()
+                animationView.animation = Animation.named("launchAnimation")
+                animationView.contentMode = .scaleAspectFit
+                animationView.frame = view.bounds
+                animationView.loopMode = .loop
+               
+                animationView.play()
+                view.addSubview(animationView)
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 4) { [weak self] in
+                    guard let self = self else {return}
+                    self.performSegue(withIdentifier: "launchScreen", sender: self)
+                }
         // Do any additional setup after loading the view.
     }
 
